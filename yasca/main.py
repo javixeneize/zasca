d6df8dca4f6a66b203e3aacd9c8ec7583155bbdd
javixeneize <javi.xeneize@gmail.com>
2022-05-15 21:39:02 +0100

    Added functionality to suppress false positives

--- yasca/main.py
+++ yasca/main.py
@@ -15,19 +15,29 @@ def scan_maven(filepath, ignore_dev):
             mavenscan.validate_vulnerable_version(advisories, dependency.get('package'), dependency.get('version'))
     return mavenscan.advisory_list, mavenscan.appname, dependencies
 
+def write_output(num_issues, unique_libraries, num_fp, qg):
+    print('{} vulnerabilities detected in {} vulnerable libraries'.format(num_issues, unique_libraries))
+    if num_fp:
+        print('{} vulnerabilities supressed'.format(num_fp))
+    print ('Quality gate passed: {}'.format(qg))
+
 @click.command()
 @click.argument('file', required=True)
 @click.option('--sbom', help='Generates CycloneDX SBOM', is_flag=True, default=False)
 @click.option('--include_dev', help='Include dev dependencies', is_flag=True, default=False)
 @click.option('--quality_gate', help='Maximum severity allowed', default='LOW')
-def run_cli(file, sbom, include_dev, quality_gate):
+@click.option('--suppression_file', help='False positives to remove')
+def run_cli(file, sbom, include_dev, quality_gate, suppression_file):
+    suppressed_items = []
     maven_data, appname, dependencies = scan_maven(file, include_dev)
-    generate_report.generate_html_report(maven_data, appname)
     if sbom:
         sbom_generator.generate_cyclonedx_sbom(dependencies)
+    if suppression_file:
+        maven_data, suppressed_items = utils.suppress_fp(maven_data, suppression_file)
+    generate_report.generate_html_report(maven_data, appname)
     unique_vuln_libraries = collections.Counter(item['package'] for item in maven_data)
-    print('{} vulnerabilities detected in {} vulnerable libraries'.format(len(maven_data), len(unique_vuln_libraries)))
     severity_data = collections.Counter(item.get('advisory').get('severity') for item in maven_data)
     qg_passed = utils.check_quality_gate(severity_data, quality_gate)
-    print ('Quality gate passed: {}'.format(qg_passed))
+    write_output(len(maven_data), len(unique_vuln_libraries), len(suppressed_items), qg_passed )
     sys.exit(not qg_passed)
+
--- yasca/utils.py
+++ yasca/utils.py
@@ -1,3 +1,5 @@
+import json
+from operator import itemgetter
 
 SEVERITY = {'CRITICAL': 4, 'HIGH': 3, 'MODERATE': 2, 'LOW': 1}
 
@@ -12,3 +14,20 @@ def check_quality_gate(severity_data, threshold):
     except TypeError:
         print("Quality gate not valid. It has to be CRITICAL, HIGH, MODERATE or LOW")
         return False
+    
+def suppress_fp(report, suppression_file):
+    suppressed_issues = []
+    try:
+        with open(suppression_file) as file:
+            fp_list = json.loads(file.read())
+        for fp in fp_list:
+            for vuln in report:
+                advisory_list = vuln.get('advisory').get('advisory').get('identifiers')
+                if vuln['package'] == fp.get('package') and fp.get('vulnerability') in map(itemgetter('value'), advisory_list):
+                    report.remove(vuln)
+                    suppressed_issues.append(vuln.copy())
+    except FileNotFoundError:
+        print ("File not found. Skipping suppression process..")
+    except json.decoder.JSONDecodeError:
+        print("JSON malformed. Skipping suppression process..")
+    return report, suppressed_issues

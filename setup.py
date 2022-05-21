324c9d9ab0fc98eac923f9799294790b2e4f00b6
Javi <javier.dominguez.jdr@gmail.com>
2022-05-21 21:25:53 +0100

    Add files via upload

--- setup.py
+++ setup.py
@@ -1,18 +1,20 @@
-from setuptools import setup
+from setuptools import setup, find_packages
+import os
 
 with open('requirements.txt') as f:
     requirements = f.read()
 
+__version__ = '0.2.0'
 setup(
     name="yasca",
-    version='0.0.1',
+    version=__version__,
     description="Yet Another SCA tool",
     author="Javier Dominguez",
-    packages=["yasca"],
+    packages=find_packages(),
     include_package_data=True,
     entry_points={
         'console_scripts': [
-            'yasca-cli=yasca.main:run_cli',
+            'yasca=yasca.main:run_cli',
         ],
     },
     install_requires=requirements,

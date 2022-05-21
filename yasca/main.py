6e092507e0fe052d8e0e4efb08082a0fbdcb6848
javixeneize <javi.xeneize@gmail.com>
2022-05-21 09:32:22 +0100

    flake8 fixes

--- yasca/main.py
+++ yasca/main.py
@@ -5,6 +5,7 @@ import collections
 import sys
 import click
 
+
 def scan_maven(filepath, ignore_dev):
     maven_tree_generator.generate_tree(filepath, ignore_dev)
     dependencies, appname = maven_tree_generator.get_dependencies()
@@ -16,11 +17,13 @@ def scan_maven(filepath, ignore_dev):
             mavenscan.validate_vulnerable_version(advisories, dependency.get('package'), dependency.get('version'))
     return mavenscan.advisory_list, mavenscan.appname, dependencies
 
+
 def write_output(num_issues, unique_libraries, num_fp, qg):
     print('{} vulnerabilities detected in {} vulnerable libraries'.format(num_issues, unique_libraries))
     if num_fp:
         print('{} vulnerabilities supressed'.format(num_fp))
-    print ('Quality gate passed: {}'.format(qg))
+    print('Quality gate passed: {}'.format(qg))
+
 
 @click.command()
 @click.argument('file', required=True)
@@ -39,5 +42,5 @@ def run_cli(file, sbom, include_dev, quality_gate, suppression_file):
     unique_vuln_libraries = collections.Counter(item['package'] for item in maven_data)
     severity_data = collections.Counter(item.get('advisory').get('severity') for item in maven_data)
     qg_passed = utils.check_quality_gate(severity_data, quality_gate)
-    write_output(len(maven_data), len(unique_vuln_libraries), len(suppressed_items), qg_passed )
+    write_output(len(maven_data), len(unique_vuln_libraries), len(suppressed_items), qg_passed)
     sys.exit(not qg_passed)

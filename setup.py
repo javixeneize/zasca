c21220a391a602a287a15261f6083f80fb721ac2
javixeneize <javixeneize@users.noreply.github.com>
2022-09-03 11:50:00 +0000

    Bumpversion changes

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 with open('requirements.txt') as f:
     requirements = f.read()
 
-__version__ = "1.0.0"
+__version__ = "2.0.0"
 
 setup(
     name="zasca",
